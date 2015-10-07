30e3d0739c1b43e661ebed00946701807fcea455
Zephyr <ixuhefeng@gmail.com>
2015-10-07 22:40:56 +0800

    main

---
+++ DataStructures/seqList_Yin/main.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "seqList_Yin.cc"
+using namespace std;
+int main() {
+	SeqList<int> sl;
+	sl.input();
+	int data = 20;
+	int i = sl.Length();
+	cout << "Insert data" << endl;
+	if (sl.Insert(i, data)) {
+		cout << "Insert successful" << endl;
+	} else {
+		cout << "Insert failed" << endl;
+	}
+	sl.output();
+	cout << "Change data" << endl;
+	data = 40;
+	sl.setData(i, data);
+	sl.output();
+	cout << "Delete data" << endl;
+	if (sl.Remove(i, data)) {
+		cout << "Remove successful" << endl;
+	} else {
+		cout << "Remove failed" << endl;
+	}
+	sl.output();
+	return 0;
+}
