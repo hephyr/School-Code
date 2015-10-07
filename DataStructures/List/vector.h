9defe7203ecfc2d86f88cb1629dc6fe7cd77f382
Zephyr <Zephyr@Zephyr.local>
2015-10-07 19:22:59 +0800

    new

--- DataStructures/List/vector.h
+++
@@ -1,9 +0,0 @@
-//
-//
-//
-//	vector.h
-//	Created by Hefeng Xu
-//	Source Code Editor Sublimbe Text
-//	GNU Compiler Collection
-//	System Mac OS X 10.10
-//
---
+++ DataStructures/seqList_Yin/main.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "seqList_Yin.cc"
+using namespace std;
+int main() {
+    SeqList<int> a;
+    return 0;
+}
+
