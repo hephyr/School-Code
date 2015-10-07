48f5a99a543b0f720a5bcf5c4755a4f81eba3888
Zephyr <Zephyr@Zephyr.local>
2015-10-07 16:53:57 +0800

    double_linkedlist

--- DataStructures/LinkedList/double_linkedlist.cpp
+++ DataStructures/LinkedList/double_linkedlist.cpp
@@ -18,29 +18,51 @@ class List {
  public:
  	class const_iterator {
  	 public:
- 	 	iterator() {}
- 	 	Object& operator*() {
- 	 		return const_iterator::retrieve();
- 	 	}
+ 	 	const_iterator() : current(nullptr) { }
  	 	const Object& operator*() const {
- 	 		return const_iterator::operator*();
- 	 	}
-
- 	 	iterator& operator++() {
- 	 		this->current = this->current->next;
  	 		return *this;
  	 	}
- 	 	iterator operator++(int) {
- 	 		iterator old = *this;
+ 	 	const_iterator operator++(int) {
+ 	 		const_iterator old = *this;
  	 		++(*this);
  	 		return old;
  	 	}
+ 	 	bool operator== (const const_iterator& rhs) const {
+ 	 		return current == rhs.current;
+ 	 	}
+ 	 	bool operator!= (const const_iterator& rhs) const {
+ 	 		return !(*this == rhs);
+ 	 	}
  	 protected:
- 	 	iterator(Node *p):const_iterator(p) {}
+ 	 	Node *current;
+ 	 	Object& retrieve() const {
+ 	 		return current->data;
+ 	 	}
+ 	 	const_iterator(Node *p) : current(p) { }
  	 	friend class List<Object>;
  	};
  	class iterator : public const_iterator {
-
+ 	 public:
+ 	 	iterator() { }
+        Object& operator*() {
+            return const_iterator::retrieve();
+        }
+        const Object& operator*() const {
+            return const_iterator::operator*();
+        }
+        
+        iterator& operator++() {
+            this->current = this->current->next;
+            return *this;
+        }
+        iterator operator++(int) {
+            iterator old = *this;
+            ++(*this);
+            return old;
+        }
+    protected:
+        iterator(Node *p):const_iterator(p) {}
+        friend class List<Object>;
  	};
  public:
  	List() {
