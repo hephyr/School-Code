a698025bb056ee120ec72dd8ed1810fea801a94f
Zephyr <ixuhefeng@gmail.com>
2015-10-22 15:11:43 +0800

    add

--- DataStructures/HomeWork6/mian.cc
+++ DataStructures/HomeWork6/mian.cc
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <string>
 #include <vector>
+#include <list>
 using namespace std;
 int main() {
-	
+	list<string> strL;
+    vector<int> v;
+    string str;
+    cout << "Please input the list element" << endl;
+    while (cin >> str)
+        strL.push_back(str);
+    cin.clear();
+    int inputNum;
+    cout << "Please input the vector element." << endl;
+    while (cin >> inputNum)
+        v.push_back(inputNum);
+    for (auto val : v) {
+        auto itrL = strL.begin();
+        for (int i = 0; i != val-1; ++i, ++itrL);
+        cout << *itrL << endl;
+    }
 	return 0;
 }
