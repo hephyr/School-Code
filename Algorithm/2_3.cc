22d4a11b39005720b9f5146c380e34d74e83bfdf
Zephyr <ixuhefeng@gmail.com>
2016-03-19 10:58:41 +0800

    version 2 fix a overflow bug

--- Algorithm/2_3.cc
+++ Algorithm/2_3.cc
@@ -9,14 +9,23 @@ int main() {
     vector<int> a = {1, 2, 3, 4, 5, 7, 8};
     int x;
     cin >> x;
-    int left = 0, right = a.size();
+    int left = 0, right = a.size() - 1;
     int find = BinarySearch(a, x, left, right);
     if (find != -1)
         cout << "Ha Ha Ha I find it" << endl;
     else {
         cout << "55555 Not find" << endl;
-        cout << "But the less number is " << a[right] << endl;
-        cout << "and the big number is " << a[left] << endl;
+        if (right < 0) {
+            cout << "the closest bigger number is " << a[left] << endl;
+            cout << "no smaller number than it" << endl;
+        }
+        else if (left >= a.size()) {
+            cout << "the closest smaller number is " << a[right] << endl;
+            cout << "no bigger number than it" << endl;
+        } else {
+            cout << " the closest smaller number is " << a[right] << endl;
+            cout << "the closest bigger number is " << a[left] << endl;
+        }
     }
     return 0;
 }
