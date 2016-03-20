b8767da318100132ba96e2d68a12ce6b98bf97bc
Zephyr <ixuhefeng@gmail.com>
2016-03-20 16:56:38 +0800

    success in 2_8

--- Algorithm/2_8.cc
+++ Algorithm/2_8.cc
@@ -2,7 +2,7 @@
 using std::endl; using std::cout;
 using std::cin;
 
-template <typename T> 
+template <typename T>
 void exchange(T a[], int left, int right, int k);
 
 template <typename T>
@@ -11,41 +11,33 @@ void sp(T a[], int left, int right, int amount);
 
 int main(int argc, char *argv[]) {
     int a[5] = {1, 2, 3, 4, 5};
-    exchange(a, 0, 4, 2);
+    exchange(a, 0, 4, 5);
     for (int i = 0; i < 5; ++i) {
         cout << a[i] << endl;
     }
+    system("pause");
     return 0;
 }
 
 template <typename T>
 void exchange(T a[], int left, int right, int k) {
-    if (left >= right)
+    if (left >= right || k > right)
         return;
     if (k - left < right - k + 1) {
-        sp(a, left, right - k + 1, k - left);
-        exchange(a, left, right - k, right - k);
+        sp(a, left, left + right - k + 1, k - left);
+        exchange(a, left, left + right - k, left + right - k);
     } else if (k - left > right - k + 1) {
-        sp(a, left, right - k + 1, right - k + 1);
+        sp(a, left, k, right - k + 1);
         exchange(a, left + right - k + 1, right, k);
     } else {
         sp(a, left, k, right - k + 1);
     }
-    // if (k > middle) {
-    //     sp(a, left, k, right - k);
-    //     exchange(a, left + right - k, right, k);
-    // } else if (k < middle) {
-    //     sp(a, left, left + right -k, k - left);
-    //     exchange(a, k, right, right - k);
-    // } else {
-    //     sp(a, left, k, (right - left) / 2);
-    // }
 }
 
 template <typename T>
 void sp(T a[], int start_a, int start_b, int amount) {
     int temp;
-    for (int i = 0; i < amount; ++ i) {
+    for (int i = 0; i < amount; ++i) {
         temp = a[start_a + i];
         a[start_a + i] = a[start_b + i];
         a[start_b + i] = temp;
