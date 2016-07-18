af47761efd810a9afafdde9ca0e95727849b67dd
Zephyr Hsu <ixuhefeng@gmail.com>
2016-07-18 21:28:08 +0800

    sort out

--- SQL_HW/couse-design/test.py
+++ SQL_HW/couse-design/test.py
@@ -1,14 +1,24 @@
 from sqlalchemy import Column, String, create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
 Base = declarative_base()
 
+
 class User(Base):
+
     __tablename__ = 'user'
+
 
     id = Column(String(20), primary_key=True)
     name = Column(String(20))
 
-engine = create_engine('mysql+mysqlconnector://root:qwer1234@localhost:3306/test')
+
+engine = create_engine('mysql+mysqlconnector://root:default@localhost:3306/test')
+
 DBSession = sessionmaker(bind=engine)
+
+session = DBSession()
+new_user = User(id='5', name='Bob')
+session.add(new_user)
+session.commit()
+session.close()
