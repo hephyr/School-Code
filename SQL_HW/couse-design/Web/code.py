cf888b404f2f4425682fbb0562ef51612ff7306f
Zephyr <ixuhefeng@gmail.com>
2016-07-18 10:00:11 +0800

    add web

---
+++ SQL_HW/couse-design/Web/code.py
@@ -0,0 +1,15 @@
+import web
+
+render = web.template.render('templates/')
+
+urls = (
+    '/(.*)', 'index'
+)
+
+class index:
+    def GET(self, name):
+        return render.hello(name) 
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
