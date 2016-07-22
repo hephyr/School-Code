414df27ef6bd36b6b656690ce2f8cdddb0e1a2e7
Zephyr <ixuhefeng@gmail.com>
2016-07-22 11:48:40 +0800

    ...

--- SQL_HW/couse-design/create_sql.sql
+++ SQL_HW/couse-design/create_sql.sql
@@ -1,3 +1,19 @@
+CREATE TABLE Class
+(
+    class_id       int unsigned    NOT NULL     AUTO_INCREMENT,
+    class_name     char(50)        NOT NULL,
+    PRIMARY KEY (class_id)
+) ENGINE=InnoDB;
+
+CREATE TABLE Sports
+(
+    sports_id       int unsigned    NOT NULL    AUTO_INCREMENT,
+    sports_name     char(50)        NOT NULL,
+    referee_id      int unsigned    NOT NULL,
+    num_athletes    tinyint unsigned    NOT NULL,
+    PRIMARY KEY (sports_id)
+) ENGINE=InnoDB;
+
 CREATE TABLE Athletes
 (
     athletes_id     int unsigned    NOT NULL    AUTO_INCREMENT,
